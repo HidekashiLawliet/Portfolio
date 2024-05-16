d8df52d071d19597688705777908f8022399d848
Hidekashi <hideaki.lawliet@proton.me>
2024-05-16 20:21:21 +0200

    ADD {adding mail form verification and waiting message}

--- contact/contact.js
+++ contact/contact.js
@@ -5,25 +5,42 @@ const name = document.querySelector('.name');
 const object = document.querySelector('.object');
 const message = document.querySelector('.coreText');
 
-const emailValue = email.value.trim();
-const nameValue = name.value.trim();
-const messageValue = message.value.trim();
-const objectValue = object.value.trim();
 
+function ValidateEmail(input) {
 
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (input.value.match(validRegex)) {
+        return true;
+    } else {
+        alert("Invalid email address!");
+        return false;
+    }
+
+}
 
 function loading() {
+    const emailValue = email.value.trim();
+    const nameValue = name.value.trim();
+    const messageValue = message.value.trim();
+    const objectValue = object.value.trim();
+    // return;
+    if (ValidateEmail(email) === false) {
+        console.log("Email is not valid");
+        return;
+    }
     if (emailValue === '' || nameValue === '' || messageValue === '' || objectValue === '') {
         alert('All fields must be filled out.');
-        return false;
+        console.log(emailValue);
+        return;
     } else {
+        isEmailSend = true;
         loadingContainer.style.display = 'block';
         console.log(emailValue);
-        isEmailSend = true;
     }
-    if (isEmailSend === true) {
-        windows.alert('Your mail as been successfully sent');
-    } else {
-        window.alert(`Sorry a problem as occured and your mail didn't upload.`);
-    }
+}
+
+if (isEmailSend === true) {
+    windows.alert('Your mail as been successfully sent');
+} else {
+    window.alert(`Sorry a problem as occured and your mail didn't upload.`);
 }
